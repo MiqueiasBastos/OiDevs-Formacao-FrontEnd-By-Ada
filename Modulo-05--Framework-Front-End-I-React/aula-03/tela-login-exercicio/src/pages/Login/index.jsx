4a8538eef54049570f0c88acf09e172944f5b841
Miquéias Bastos <miqueiasbastos@hotmail.com>
2023-01-18 22:10:38 -0300

    Exercício Autenticação

--- Modulo-05--Framework-Front-End-I-React/aula-03/tela-login-exercicio/src/pages/Login/index.jsx
+++ Modulo-05--Framework-Front-End-I-React/aula-03/tela-login-exercicio/src/pages/Login/index.jsx
@@ -11,19 +11,54 @@ import Subtitle from "../../components/Subtitle";
 const Login = () => {
     const navigate = useNavigate();
     const [title, setTitle] = useState("Login");
-    const [user, setUser] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [userDatabase, setUserDatabase] = useState([
+        {
+            username: 'miqueias',
+            password: '123'
+        },
+        {
+            username: 'raniel',
+            password: 'oidevs'
+        }
+    ])
+
+    function handleLogin(){
+        let auth = false;
+
+        userDatabase.forEach(user=>{
+            if(user.username === username && user.password === password) {
+                auth = true;
+            }
+        })
+
+        if(auth) {
+            navigate("/home")
+        } else {
+            window.alert("Usuário ou senha incorretos");
+        }
+
+
+    }
     return (
         <div className="container">
             <div className="box-login">
                 <Title text={title} />
-                {user && <Subtitle text={`Usuário: ${user}`} />}
+                {username && <Subtitle text={`Usuário: ${username}`} />}
                 <Input
                     label="Usuário"
-                    value={user}
-                    onChange={(e) => setUser(e.target.value)}
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                 />
-                <Input label="Senha" hideContent />
-                <Button title="Entrar" onClick={() => navigate("/home")} />
+                <Input
+                    label="Senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    hideContent
+                />
+                <Button title="Entrar" onClick={handleLogin} />
                 <Button title="Alterar Título" onClick={() => setTitle("Novo Titulo")} bgColor="blue" />
                 <Link text="Esqueceu a senha?" url="https://google.com" />
             </div>
